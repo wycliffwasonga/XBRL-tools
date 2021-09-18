bde6ad24604098cdbd27997d30a1f0d36b52de5f
Wycliff Wasonga <74678465+wycliffwasonga@users.noreply.github.com>
2021-09-18 15:51:26 +0300

    nx g @nrwl/angular:app ixbrl-viewer

---
+++ apps/xbrl-viewer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xbrl-tools-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'xbrl-viewer';
+}
