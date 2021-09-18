47202732ff17fec6f29b2add5b431aef30bee336
Wycliff Wasonga <74678465+wycliffwasonga@users.noreply.github.com>
2021-09-18 18:33:45 +0300

    update test

--- apps/xbrl-viewer/src/app/app.component.spec.ts
+++ apps/xbrl-viewer/src/app/app.component.spec.ts
@@ -1,16 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let de: DebugElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    de = fixture.debugElement;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should render xbrl-tool-viewer component', () => {
+    const el = de.query(By.css('xbrl-tool-viewer')).nativeElement;
+    expect(el).toBeTruthy();
   });
 });
